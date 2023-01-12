966f9edc56e29a0e0aff0a7fdca3bcef952c4c4c
Selam Yihew <selamyihew@macbook-pro-2.gateway.sonic.net>
2023-01-11 20:11:52 -0800

    git commit -m

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
+
 import logo from './logo.svg';
 import './App.css';
 
+const todoList = [
+  {
+  title: 'Complete assignment',
+  objectID: 0,
+  },
+  {
+  title: 'Read Road to React',
+  objectID: 1,
+  },
+  {
+  title: 'Watch videos',
+  objectID: 2,
+  }
+  ];
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Todo List</h1>
+        <ul>
+          {todoList.map(function (item){
+            return <li key={item.objectID}>
+              {item.title}
+              </li>
+          })}
+        </ul>
     </div>
   );
 }
